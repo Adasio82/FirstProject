29da11f21b4d79271be938cd1c819539ac9ed4af
Adasio82 <adam.orlowski.poczta@wp.pl>
2018-03-17 18:58:12 +0100

    first commit

---
+++ src/main/java/com/mycompany/Main.java
@@ -0,0 +1,22 @@
+package com.mycompany;
+
+public class Main {
+
+    //psvm
+    public static void main(String[] args) {
+
+        int number1 = 342342343;
+        int number2 = 1024234234;
+        int result;
+
+        String word1 = "Hello ";
+        String word2 = "World";
+
+        result = number1 + number2;
+        //sout
+        System.out.println(result);
+        System.out.println(word1 + word2);
+    }
+
+
+}
